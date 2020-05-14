78b86c260ae246503bd50a79c46b2ac009585c3b
bguina <benoit.guina@gmail.com>
2020-05-14 13:41:11 +0200

    [wip]:WowNavigator: added commented navigation commands

--- coreDLL/WowNavigator.h
+++ coreDLL/WowNavigator.h
@@ -11,6 +11,24 @@ public:
 	){}
 
 protected:
+    /*HWND wowWindow = FindWindow(NULL, _T("World of Warcraft"));
+
+            //0x57 -> W
+            //0x51 -> Q
+            //0x41 -> A
+
+            //PostMessage(wowWindow, WM_KEYDOWN, 0x41, 0x00110001); //Flags found with spy++
+            //PostMessage(notepad, WM_CHAR, 'w', 0);
+            //Sleep(2000);
+            //PostMessage(wowWindow, WM_KEYUP, 0x41, 0xC0110001);  //Flags found with spy++
+
+
+
+            //SendMessage(notepad, WM_KEYDOWN, 'w', 0);
+            //Sleep(3000);
+
+            //PostMessage(notepad, WM_CHAR, 'w', 0);
+    */
 
 };
 
